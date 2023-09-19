d8bfaaa78971da0731b046df8437f051bb9b521d
damiencot <damien.cot@gmail.com>
2023-09-19 10:42:11 +0200

    error

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,18 @@
 
 using namespace std;
 
+void undefinedFunction(); // Déclaration de fonction sans définition
+
 int main(int argc, char* argv[]) {
     int num = argc - 1;
-    
+
     if (num == 0) {
         cout << "No arguments provided\n";
     }
     if (num == 0) {
         cout << "No arguments provided\n";
     }
-    else if (num == 0) { // intentional mistake
+    else if (num == 0) { // Intentional mistake
         cout << "1 argument provided\n";
     }
     else if (num == 2) {
@@ -20,13 +22,20 @@ int main(int argc, char* argv[]) {
     else {
         cout << num << " arguments provided\n";
     }
-    if (argv != 0) {
-        cout << "argv not null\n"; // intentional extra-semicolon
+
+    // Utilisation incorrecte de argv
+    if (argv[argc]) {
+        cout << "Invalid access to argv\n";
     }
-    if (argv == nullptr) {
-        return **argv; // intentional nullptr dereference
-    }
+
+    // Utilisation de variables non initialisées et non déclarées
+    int uninitializedVar;
+    cout << uninitializedVar << endl; // Utilisation d'une variable non initialisée
+    int undeclaredVar;
+    cout << undeclaredVar << endl; // Utilisation d'une variable non déclarée
+
+    // Appel d'une fonction non définie
+    undefinedFunction();
 
     return 0;
 }
-
